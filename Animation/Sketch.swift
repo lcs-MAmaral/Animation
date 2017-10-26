0b8fddab76b7fda70d809c2f58878c2484a9c8bf
Matthew Amaral <mamaral@lcs-mba3204.lcs.on.ca>
2017-10-26 11:03:03 -0400

    Got the circles and now need the negatives

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,6 +8,8 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var dx : Int
+    var offset : Int
     
     // This function runs once
     override init() {
@@ -17,17 +19,32 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
+        dx = 2
+        offset = 250
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
+        canvas.drawShapesWithBorders = false
+    
+        
         
         // Change position
-        x += 1
-        
+        offset += 1
+      
+        canvas.fillColor = Color.black
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: offset, centreY: 50, width: 50, height: 50)
+        canvas.fillColor = Color.yellow
+        canvas.drawEllipse(centreX: 250 - offset, centreY: 150, width: 50, height: 50)
+        canvas.fillColor = Color.orange
+        canvas.drawEllipse(centreX: offset, centreY: 250, width: 50, height: 50)
+        canvas.fillColor = Color.red
+        canvas.drawEllipse(centreX: 250 - offset, centreY: 350, width: 50, height: 50)
+        canvas.fillColor = Color.purple
+        canvas.drawEllipse(centreX: offset, centreY: 450, width: 50, height: 50)
         
     }
     
